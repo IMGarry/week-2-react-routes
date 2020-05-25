b7c1d7f08548cc79650e1ac9e84bfde1472c2887
Igor Marchenko <igor.ms83@gmail.com>
2020-05-25 22:01:41 +0300

    Commit homework 11-2 v3-1

--- client/components/profile.js
+++ client/components/profile.js
@@ -10,7 +10,7 @@ const Profile = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <div id="title">Profile</div>
-          <div id="061f2100-c32d-461a-9f6c-20e6535a4365">{user}</div>
+          <div id="username">{user}</div>
           <Link to="/dashboard"> Go To Root</Link>
           <Link to="/dashboard/main"> Go To Main</Link>
         </div>
